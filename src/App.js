452c8206fce88b248779b7507a5adc02e5b31a1b
Saidov Yosin <saidovyosin2004@gmail.com>
2022-03-26 15:21:13 +0300

    still

--- src/App.js
+++ src/App.js
@@ -1,58 +1,88 @@
 import "./App.css";
+import React from "react";
+import Array from "./Components/Props"
 import {useState}  from "react"
 import H1 from "./Components/H1";
 
 const initialState = []
-
+// const arr = [
+//   {
+//     name: "alex",
+//     age: 17
+//   },
+//   {
+//     name: "john",
+//     age: 12
+//   }
+// ]
 function App() {
-  const[text, setText] = useState("")
-  const[todos, setTodos] = useState(initialState)
+//    return  <Array arr={arr} />
+    const [count, setCount] = useState(0)
   
-  const handleChange = (e) => {
-    setText(e.target.value)
-  }
-  const addTodo = () =>{   
-    if(text == ""){
-      return
-    }
-    setTodos(function(prev){
-      let newTodo = {
-        text:text,
-        isCompleted:false
-      }
-      return [...prev, newTodo]
-    })
-    setText("")
+    return <div>
+      <p>{count}</p>
+      <button onClick={() => setCount(count + 1)}>+</button>
+      <button onClick={() => setCount(count - 1)}>-</button>
+      <button onClick={() => setCount(count * count)}>double</button>
+      <button onClick={() => setCount(count * 0)}>anew</button>
+    </div>
+     
+
+  // const[text, setText] = useState("")
+  // const[todos, setTodos] = useState(initialState)
+  
+  // const handleChange = (e) => {
+  //   setText(e.target.value)
+  // }
+  // const addTodo = () =>{   
+  //   if(text == ""){
+  //     return
+  //   }
+  //   setTodos(function(prev){
+  //     let newTodo = {
+  //       text:text,
+  //       isCompleted:false
+  //     }
+  //     return [...prev, newTodo]
+  //   })
+  //   setText("")
     
-  }
-  const remove = () => {
-
-  }
-  function hello(props){
-    return<h1>Kak ti tam, {props.name}</h1>
-  }
-  console.log(hello)
-
-   return (
-     <>
-     <div className="app">
-      <H1 text="Todo App" />
-     <div>
-       <div className="todo-list">
-         {todos.map((elem, index ) => {
-           return(
-             <div className="todo" key={index}>
-               {elem.text}
-               <button onClick={remove}>Remove</button>e'
-             </div>
-           )
-         })}
-         <input onChange={handleChange} value={text}/>
-         <button onClick={addTodo}>Add</button>
-       </div>
-     </div>
-     </div>
-     </>
-   )
+  // }
+  // const Completed = (index) => {
+  //   const copy = [...todos]
+  //   copy[index].isCompleted = copy[index].isCompleted ? false : true
+  //   setTodos(copy)
+  // }
+  // const Delate = (index) => {
+  // const copy = [...todos]
+  // copy.splice(index, 1)
+  // setTodos(copy)
+  // }
+  
+ 
+  //  return (
+  //    <>
+  //    <div className="app">
+  //     <H1 text="Todo App" />
+  //    <div>
+  //      <div className="todo-list">
+  //        {todos.map((elem, index ) => {
+  //          return(
+  //            <div className="todo" key={index}>
+  //              <p style={elem.isCompleted ? { textDecoration: "line-through"}:{}}>
+  //              {elem.text}
+  //              </p>
+  //              <button onClick={() => Completed(index)}>Completed</button>
+  //              <button onClick={() => Delate(index)}>Delate</button>
+  //            </div>
+  //          )
+  //        })}
+  //        <input onChange={handleChange} value={text}/>
+  //        <button onClick={addTodo}>Add</button>
+  //      </div>
+  //    </div>
+  //    </div>
+  //    </>
+  //  )
 }
 export default App;
