5fb2d3aa2a15c2d25236fa4366879de6a28f39f6
Saidov Yosin <saidovyosin2004@gmail.com>
2022-04-21 17:04:15 +0300

    handleChange

--- src/App.js
+++ src/App.js
@@ -1,88 +1,24 @@
+import { useState } from "react";
 import "./App.css";
-import React from "react";
-import Array from "./Components/Props"
-import {useState}  from "react"
-import H1 from "./Components/H1";
+import Chart from "./Components/Chart";
+import Dashboard from "./Components/Dashboard";
+import Deposits from "./Components/Deposits";
+import listItems from "./Components/listItems"
+import Orders from "./Components/Orders";
+import Title from "./Components/Title"
 
-const initialState = []
-// const arr = [
-//   {
-//     name: "alex",
-//     age: 17
-//   },
-//   {
-//     name: "john",
-//     age: 12
-//   }
-// ]
+
 function App() {
-//    return  <Array arr={arr} />
-    // const [count, setCount] = useState(0)
-  
-    // return <div>
-    //   <p>{count}</p>
-    //   <button onClick={() => setCount(count + 1)}>+</button>
-    //   <button onClick={() => setCount(count - 1)}>-</button>
-    //   <button onClick={() => setCount(count * count)}>double</button>
-    //   <button onClick={() => setCount(count * 0)}>anew</button>
-    // </div>
-     
+return(
+    <>
+    <Chart/>
+    <Dashboard/>
+    <Deposits/>
+    <listItems/>
+    <Orders/>
+    <Title/>
+    </>
+)
+}
 
-const [text, setText] = useState("")
-const [todos, setTodos] = useState(initialState)
-  
-const handleChange = (e) =>{
-  setText(e.target.value)
-}
-  const addTodo = () =>{   
-    if(text == ""){
-      return
-    }
-    setTodos(function(prev){
-      let newTodo = {
-        text:text,
-        isCompleted:false
-      }
-      return [...prev, newTodo]
-    })
-    setText("")
-    
-  }
-  const Completed = (index) => {
-    const copy = [...todos]
-    copy[index].isCompleted = copy[index].isCompleted ? false : true
-    setTodos(copy)
-  }
-  const Delate = (index) => {
-  const copy = [...todos]
-  copy.splice(index, 1)
-  setTodos(copy)
-  }
-  
- 
-   return (
-     <>
-     <div className="app">
-      <H1 text="Todo App" />
-     <div>
-       <div className="todo-list">
-         {todos.map((elem, index ) => {
-           return(
-             <div className="todo" key={index}>
-               <p style={elem.isCompleted ? { textDecoration: "line-through"}:{}}>
-               {elem.text}
-               </p>
-               <button onClick={() => Completed(index)}>Completed</button>
-               <button onClick={() => Delate(index)}>Delate</button>
-             </div>
-           )
-         })}
-         <input onChange={handleChange} value={text}/>
-         <button onClick={addTodo}>Add</button>
-       </div>
-     </div>
-     </div>
-     </>
-   )
-}
 export default App;
